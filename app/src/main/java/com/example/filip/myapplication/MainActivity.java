8617154d05e71f4de5044438726e30de780c7f96
Kuba <guardofshadow57@gmail.com>
2018-02-08 11:56:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/filip/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.filip.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView web = (WebView) findViewById(R.id.strona);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.loadUrl("file:///android_res/raw/abc.html");
+        web.addJavascriptInterface(new WebAppInterface(this),"Android");
+
+
+    }
+}
---
+++ app/src/main/java/com/example/filip/myapplication/WebAppInterface.java
@@ -0,0 +1,38 @@
+package com.example.filip.myapplication;
+
+import android.content.Context;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Kuba on 23.01.2018.
+ */
+
+public class WebAppInterface {
+    Context mContext;
+    Map<Integer, Integer> mapa = new HashMap<Integer,Integer>();
+    int b=1;
+    int c=1;
+
+    /** Instantiate the interface and set the context */
+    WebAppInterface(Context c) {
+        mContext = c;
+    }
+
+    /** Show a toast from the web page */
+    @JavascriptInterface
+    public void showToast(String toast) {
+        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+    }
+
+    @JavascriptInterface
+    public void silnia() {
+        b = b * c;
+        Toast.makeText(mContext, "" + b, Toast.LENGTH_LONG).show();
+        c=c+1;
+
+    }
+}
